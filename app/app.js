bd64b260cccdce675be6a23244fd04b3c496fdc3
Micaela Villordo <mvillordo@fi.uba.ar>
2023-09-21 21:00:17 -0300

    Agrego archivo de config (para datos de apis externas)

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { nanoid } from 'nanoid';
 import axios from 'axios';
+import { spaceflightNewsConfig } from './config.js';
 
 const app = express();
 
@@ -12,15 +13,8 @@ app.use((req, res, next) => {
 });
 
 app.get('/spaceflight_news', async (req, res) => {
-    const response = await axios.get("https://api.spaceflightnewsapi.net/v3/articles?_limit=5");
-    
-    let titles = [];
-    response.data.forEach(element => {
-        if (element.hasOwnProperty('title')) {
-            titles.push(element.title);
-        }
-    });
-
+    const response = await axios.get(`${spaceflightNewsConfig.url}?_limit=${spaceflightNewsConfig.limit}`);
+    const titles = response.data.map(({ title }) => title).filter(Boolean);
     res.status(200).send(titles);
 })
 
---
+++ app/config.js
@@ -0,0 +1,4 @@
+export const spaceflightNewsConfig = {
+    url: "https://api.spaceflightnewsapi.net/v3/articles",
+    limit: 5
+};
