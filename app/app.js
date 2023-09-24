c16ba8be53ae6187f98ac0bb71d453d30d6a9748
MaximilianoCovela <mcovela@fi.uba.ar>
2023-09-23 20:15:06 -0700

    Se agregan endpoints '/ping' y 'quote'

--- app/app.js
+++ app/app.js
@@ -2,15 +2,20 @@ import express from 'express';
 import { nanoid } from 'nanoid';
 import axios from 'axios';
 import { spaceflightNewsConfig } from './config.js';
+import { quoteConfig } from './config.js';
 
 const app = express();
-
 const id = nanoid();
+const port = 3000
 
 app.use((req, res, next) => {
     res.setHeader('X-API-Id', id);
     next();
 });
+
+app.get('/ping', (req, res) => {
+    res.status(200).send("Respuesta exitosa");
+})
 
 app.get('/spaceflight_news', async (req, res) => {
     const response = await axios.get(`${spaceflightNewsConfig.url}?_limit=${spaceflightNewsConfig.limit}`);
@@ -18,4 +23,20 @@ app.get('/spaceflight_news', async (req, res) => {
     res.status(200).send(titles);
 })
 
-app.listen(3000, () => console.log("Escuchando en el puerto", 3000))
+app.get('/quote', async (req, res) => {
+    const response = await axios.get(`${quoteConfig.url}`)
+    var jsonResult = CreateJsonResult(response);
+    res.status(200).send(jsonResult);
+})
+
+app.listen(port, () => console.log("Escuchando en el puerto", port))
+
+/***************************
+    FUNCIONES AUXILIARES
+***************************/
+function CreateJsonResult(response){
+    var jsonResult = {};
+    jsonResult["content"] = response.data.content;
+    jsonResult["author"] = response.data.author
+    return jsonResult;
+}
--- app/config.js
+++ app/config.js
@@ -2,3 +2,7 @@ export const spaceflightNewsConfig = {
     url: "https://api.spaceflightnewsapi.net/v3/articles",
     limit: 5
 };
+
+export const quoteConfig = {
+    url: "https://api.quotable.io/random"
+};
