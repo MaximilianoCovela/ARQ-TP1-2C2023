7a7e2c6e1d017b77825ad8a84bc55dd695580cbd
Vitiligio <mpalazon3@gmail.com>
2023-09-25 01:31:13 -0300

    Metar API logic added with some error handleling

--- app/app.js
+++ app/app.js
@@ -4,6 +4,9 @@ import axios from 'axios';
 import { spaceflightNewsConfig } from './config.js';
 import { quoteConfig } from './config.js';
 
+import { XMLParser } from 'fast-xml-parser';
+import { decode } from 'metar-decoder';
+
 const app = express();
 const id = nanoid();
 const port = 3000
@@ -27,6 +30,23 @@ app.get('/quote', async (req, res) => {
     const response = await axios.get(`${quoteConfig.url}`)
     var jsonResult = CreateJsonResult(response);
     res.status(200).send(jsonResult);
+})
+
+app.get('/metar', async (req, res) => {
+    const station = req.query.station;
+    const parser = new XMLParser();
+    const response = await axios.get(`https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=${station}&hoursBeforeNow=1`);
+    const parsed = parser.parse(response.data);
+
+    if (parsed && parsed.response && parsed.response.data && parsed.response.data.METAR && parsed.response.data.METAR.raw_text) {
+      const rawText = parsed.response.data.METAR.raw_text;
+
+      decode(rawText);
+      
+      res.send(`${rawText}`);
+    } else {
+      res.status(404).send(`METAR data not found for station ${station}`);
+    }
 })
 
 app.listen(port, () => console.log("Escuchando en el puerto", port))
