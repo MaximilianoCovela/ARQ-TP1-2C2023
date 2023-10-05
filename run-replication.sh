f06facb4f68397107d517ebff4359c62e56574af
Micaela Villordo <mvillordo@fi.uba.ar>
2023-10-05 02:55:18 -0300

    Modify replication files

--- run-replication.sh
+++ run-replication.sh
@@ -1,3 +1,3 @@
 alias docc='docker compose -f docker-compose-replication.yml' # Se puede necesitar cambiarlo a 'docker-compose -f docker-compose-replication.yml' según cómo esté instalado.
 
-docc --compatibility up -d
+docc --compatibility up -d --scale node=3
