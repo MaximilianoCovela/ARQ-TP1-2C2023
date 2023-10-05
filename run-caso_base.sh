6ed25ef6df6d118ced5d3ee43c368a5e2066cca2
MaximilianoCovela <mcovela@fi.uba.ar>
2023-10-05 00:36:15 -0300

    Se agregan archivos de configuracion para cada tactica

---
+++ run-caso_base.sh
@@ -0,0 +1,3 @@
+alias docc='docker compose -f docker-compose-caso_base.yml' # Se puede necesitar cambiarlo a 'docker-compose -f docker-compose-caso_base.yml' según cómo esté instalado.
+
+docc --compatibility up -d
